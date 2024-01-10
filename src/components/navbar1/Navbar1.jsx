647a061f9384ea6576e6a060ca401db0ef54af74
aditya pawar <113058497+adityaPawar1999@users.noreply.github.com>
2024-01-10 18:20:09 +0530

    Update Navbar1.jsx

--- src/components/navbar1/Navbar1.jsx
+++ src/components/navbar1/Navbar1.jsx
@@ -10,23 +10,7 @@ import TwitterIcon from '@mui/icons-material/Twitter';
 
 export default function Navbar1() {
     return (
-        <div classname='container'>
-            <div className={style.main}>
-                <div className={`${style.navbar1} container row g-0`}>
-                    <div className={`${style.socialSite} col-lg-8 col-md-4 col-sm-4  `}>
-                        <FacebookRoundedIcon/>
-                        <InstagramIcon/>
-                        <YouTubeIcon/>
-                        <TwitterIcon/>
-                    </div>
-                    <div className={`${style.link} col-lg-3 col-md-8 col-sm-8 `}>
-                        <Link  className={style.link1} to='/'>Contact</Link>
-                        <Link className={style.link1} to='/'>AboutUs</Link>
-                        <Link  className={style.link1} to='/'>Login</Link>
-                    </div>
-                </div>
-            </div>
 
-        </div>
+        <></>
     )
 }
