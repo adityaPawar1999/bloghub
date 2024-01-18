d7919513aadfac5ce7527035b05874d010a3af97
aditya pawar <113058497+adityaPawar1999@users.noreply.github.com>
2024-01-18 18:40:07 +0530

    Update Navbar1.jsx

--- src/components/navbar1/Navbar1.jsx
+++ src/components/navbar1/Navbar1.jsx
@@ -1,11 +1,5 @@
 
 import { Link } from 'react-router-dom'
-import style from './navbar1.module.css'
-
-import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import YouTubeIcon from '@mui/icons-material/YouTube';
-import TwitterIcon from '@mui/icons-material/Twitter';
 
 
 export default function Navbar1() {
